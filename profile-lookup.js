695ca9016b036bc531b66ec59f3c349d9d67e7b5
zinovdim <zinovdim@yandex.ru>
2023-01-15 21:46:11 +0300

    add new JS files

---
+++ profile-lookup.js
@@ -0,0 +1,39 @@
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(firstName, prop) {    
+    var result = contacts.filter(x => x.firstName == firstName)
+
+    if (result.length === 0) {
+        return "No such contact";
+    } else {
+        return result[0][prop] ? result[0][prop] : "No such property";
+    }
+}
+
+lookUpProfile("Akira", "likes");
