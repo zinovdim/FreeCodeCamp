a13fe84a7d9e3b1ff3a505c54f09736e05eb0224
zinovdim <zinovdim@yandex.ru>
2023-02-09 21:23:15 +0300

    add new js file

---
+++ create-strings-using-template-literals.js
@@ -0,0 +1,19 @@
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+   const failureItems = [];
+   
+     arr.forEach((num) => {
+    failureItems.push(`<li class="text-warning">${num}</li>`)
+  });
+
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
