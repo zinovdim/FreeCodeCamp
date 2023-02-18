34fb60a42299e08d274d0c3c742d3775d81d4d96
zinovdim <zinovdim@yandex.ru>
2023-02-18 22:03:27 +0300

    add new JS files

---
+++ word-blanks.js
@@ -0,0 +1,9 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+let wordBlanks = "Once there was a " + myNoun + " which was very " + myAdjective + ". ";
+wordBlanks += "It " + myVerb + " " + myAdverb + " around the yard."; // Change this line
+// Only change code above this line
